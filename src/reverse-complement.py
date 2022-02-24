3ed4b83425cdd4d972c355f0da8e19104fdda2b1
[natgupo19] <[natgupo@lcg.unam.mx]>
2022-02-24 00:49:34 -0600

    Se agrega script reverse_complement

---
+++ src/reverse-complement.py
@@ -0,0 +1,26 @@
+'''
+NAME
+	reverse-complement.py
+    
+VERSION
+    1.0
+    
+AUTHOR
+	
+    
+DESCRIPTION
+	Make the reverse complement of DNA sequence
+    
+CATEGORY
+	Genomic Sequence
+    
+USAGE
+
+    % python reverse-complement.py -i 
+    
+    example
+    
+    % python reverse-complement -i
+
+'''
+
