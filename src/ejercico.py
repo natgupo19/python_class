53d9a45d8e8600bce3e2150a8bf9040af4e45526
[natgupo19] <[natgupo@lcg.unam.mx]>
2022-03-17 12:26:06 -0600

    Contador de nucleotidos

---
+++ src/ejercico.py
@@ -0,0 +1,5 @@
+dna = input("Introduzca una secuncia de DNA: \n")
+print(f"La cantidad de A es: {dna.count('A')}.");
+print(f"La cantidad de T es: {dna.count('T')}.");
+print(f"La cantidad de C es: {dna.count('C')}.");
+print(f"La cantidad de G es: {dna.count('G')}.");
