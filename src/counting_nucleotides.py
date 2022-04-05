f1677b297a8d3a02c820fbd870417b7d34d39af3
natgupo19 <99424683+natgupo19@users.noreply.github.com>
2022-04-05 11:53:05 -0500

    cambios finales...

--- src/counting_nucleotides.py
+++ src/counting_nucleotides.py
@@ -12,17 +12,21 @@ DESCRIPTION
 El programa muestra cuantas A, T, C, y G se encuentran en una secuencia de DNA introducida por el usuario.
 
 CATEGORY
+DNA sequence
 
 USAGE
+El programa permite obtener la frecuencia de aparicion de cada uno de los cuatro nucleotidos
+en una secuencia de DNA. Esta secuencia debe de ser introducida por el usuario.
 
 ARGUMENTS
+None
+
+SEE ALSO
+None
 '''
 
 # Pedimos la secuencia de DNA al usuario
 dna = input("Introduzca una secuencia de DNA: \n")
 
 # Contamos la cantidad de nucleótidos que hay en la secuencia y lo imprimimos al usuario
-print(f"La cantidad de A es: {dna.count('A')}.")
-print(f"La cantidad de T es: {dna.count('T')}.")
-print(f"La cantidad de C es: {dna.count('C')}.")
-print(f"La cantidad de G es: {dna.count('G')}.")
+print(f"La cantidad de A es: {dna.count('A')}. \nLa cantidad de T es: {dna.count('T')}. \nLa cantidad de C es: {dna.count('C')}. \nLa cantidad de G es: {dna.count('G')}.")
