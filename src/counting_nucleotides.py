3bea42176df5ef29ff33d5f95195eb0855b36e9f
[natgupo19] <[natgupo@lcg.unam.mx]>
2022-05-12 11:37:44 -0500

    mejoras de conteo

--- src/counting_nucleotides.py
+++ src/counting_nucleotides.py
@@ -25,7 +25,7 @@ None
 '''
 
 # Obtener la secuencia de ADN del usuario
-dna= input("Introduzca una secuencia de DNA:\n")
+dna= input("Introduzca una secuencia de DNA:\n").upper()
 
 # Realizar conteo de cada base
 freq_A = dna.count('A')
@@ -33,8 +33,12 @@ freq_C = dna.count('C')
 freq_G = dna.count('G')
 freq_T = dna.count('T')
 
-# Imprimir el resultado
-print(f"""La cantidad de A es: {freq_A}
+if (freq_A == 0 & freq_C == 0 & freq_G == 0 & freq_G == 0 or dna == ''):
+    print("\nNo se introdujo una secuencia de ADN.\n")
+    
+else:
+    # Imprimir el resultado
+    print(f"""\nLa cantidad de A es: {freq_A}
 La cantidad de C es: {freq_C}  
 La cantidad de G es: {freq_G}  
-La cantidad de T es: {freq_T}""")
+La cantidad de T es: {freq_T}\n""")
