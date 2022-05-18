002357655fd86e2061c63ed21f8f88e5d7951a80
[natgupo19] <[natgupo@lcg.unam.mx]>
2022-05-18 18:36:26 -0500

    actualizacion del programa

--- src/aminoacid_content.py
+++ src/aminoacid_content.py
@@ -38,3 +38,64 @@ INPUT
 SEE ALSO
     None
 '''
+
+import argparse
+
+# Crear el parser
+parser = argparse.ArgumentParser(description = "Script que calcula el \
+           contenido de una lista de aminoacidos a patir de una secuencia proteica.")
+
+# Agregar y almacenar los argumentos
+parser.add_argument("-s", "--sequence",
+                    help="Protein sequence to analyze",
+                    type=str,
+                    required=True)
+                    
+parser.add_argument("-a", "--aminoacid",
+                    help="Amino acid to search",
+                    type=str,
+                    required=True)
+
+parser.add_argument("-o", "--output",
+                    metavar="path/to/output/file",
+                    help="Path for the output file",
+                    required=False)
+                    
+parser.add_argument("-r", "--round",
+                    help="Number of digits to round",
+                    type=int,
+                    required=False)
+  
+args = parser.parse_args()
+
+def aminoacid_content(protein_sequence, aminoacid):
+    protein_length = len(protein_sequence)
+    content = (protein_sequence.count(aminoacid) *100) / protein_length
+    return(content)
+
+amino = args.aminoacid.upper()
+sequence = args.sequence.upper()
+
+percentage = aminoacid_content(sequence, amino)
+
+# Redondear el porcentaje si fue solicitado por el usuario
+if args.round:
+    percentage = round(percentage, args.round)
+
+# Imprimir el porcentaje en el archivo indicado por el usuario
+if args.output:
+    with open(args.output, 'w') as output_file:
+        print(f"Secuencia proteica: {sequence} \
+                \nAminoacido: {amino} \
+                \nContenido de {amino} en la secuencia: {percentage} %",
+                file=output_file)
+                  
+    print(f"\nSe ha generado el archivo {args.output} con \
+            el contenido de {amino}.\n")
+
+# Imprimir resultado a pantalla si no fue solicitado un archivo output
+else:
+    print(f"\nSecuencia proteica: {sequence} \
+            \nAminoacido: {amino} \
+            \nContenido de {amino} en la secuencia: {percentage} %\n")
+        
